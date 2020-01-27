708aee908a11f3a9a30130f02f241a57964b8605
Beth Applewhite <bethapplewhite.nola@gmail.com>
2020-01-27 16:45:51 -0600

    last update

--- script.js
+++ script.js
@@ -1,29 +1,31 @@
-'use strict'
+'use strict';
 
 function getDogImage() {
-    fetch('http://dog.ceo/api/breeds/image/random')
+  fetch('https://dog.ceo/api/breeds/image/random')
     .then(response => response.json())
-    .then(responseJson => console.log(responseJson));
-        displayResults(responseJson))
-    .catch(error => alert('Something went wrong. Try again later.')); 
+    .then(responseJson => 
+      displayResults(responseJson))
+    .catch(error => alert('Something went wrong. Try again later.'));
 }
 
 function displayResults(responseJson) {
-    console.log(responseJson);
-    $('.results-img').replaceWith(  
-        `<img src="${responsiveJson.message}" class="results-img">`
-    )
-    $('.results').removeClass('hidden');
+  console.log(responseJson);
+  //replace the existing image with the new one
+  $('.results-img').replaceWith(
+    `<img src="${responseJson.message}" class="results-img">`
+  )
+  //display the results section
+  $('.results').removeClass('hidden');
 }
 
 function watchForm() {
-    $('form').submit(event => {
-        event.preventDefault();
-        getDogImage();
-    });
+  $('form').submit(event => {
+    event.preventDefault();
+    getDogImage();
+  });
 }
 
 $(function() {
-    console.log('App loaded! Waiting for submit!');
-    watchForm();
+  console.log('App loaded! Waiting for submit!');
+  watchForm();
 });
